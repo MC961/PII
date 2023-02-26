06fda18f7a2b0545361f734ce9f8904a73cb4052
MC961 <123888365+MC961@users.noreply.github.com>
2023-02-26 21:26:11 +0200

    Update test.py

--- test.py
+++ test.py
@@ -7,7 +7,7 @@ class MyServer(BaseHTTPRequestHandler):
         self.send_response(200)
         self.send_header("Content-type", "text/html")
         self.end_headers()
-        self.wfile.write(bytes("<html><head><title>https://pythonbasics.org</title></head>", "utf-8"))
+        self.wfile.write(bytes("<html><head><title>our webserver</title></head>", "utf-8"))
         self.wfile.write(bytes("<p>Request: %s</p>" % self.path, "utf-8"))
         self.wfile.write(bytes("<body >", "utf-8"))
         self.wfile.write(bytes("<p>This is an example web server where we are trying to create a webserver from scratch.</p>", "utf-8"))
