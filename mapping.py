b5388cd469e4815a5f3a781875c578318088f997
MC961 <123888365+MC961@users.noreply.github.com>
2023-04-27 10:20:43 +0300

    Add files via upload

---
+++ mapping.py
@@ -0,0 +1,6 @@
+mappings = {
+    '/': 'C:/Users/Marc/Documents/VScode/webserver/datahtml/maps.html',
+    '/about': 'C:/Users/Marc/Documents/VScode/webserver/datahtml/the_team.html',
+    '/Instagram':'C:/Users/Marc/Documents/VScode/instagram/main.py',
+    '/me':'C:/Users/Marc/Documents/VScode/webserver/datahtml/website.html'
+}
